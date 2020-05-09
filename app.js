07783dcf5533bcbcbeddb2c59e1ad24d9a690397
Muhammad Usama Bin Akhtar Khan <usamakhangt4@gmail.com>
2020-05-09 05:41:11 +0500

    fixes

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-function validateEmail() {
-  let email = document.getElementById("email");
-  email = email.nodeValue;
-  alert(email);
+function validateEmail(email) {
+  if (email == "") {
+    email.preventDefault();
+  }
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
+
+function validate() {
+  var result = document.getElementById("result");
+  var email = document.getElementById("email");
+  email = email.value;
+  if (validateEmail(email)) {
+    result.innerHTML = email + " is a valid email";
+    document.getElementById("warning").style.visibility = "hidden";
+  } else {
+    document.getElementById("warning").style.visibility = "visible";
+    result.innerHTML = email + " is an invalid email";
+  }
+  return false;
+}
+
+// $("#validate").on("click", validate);
